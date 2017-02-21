68547cf30520242101dcae63061f299d8965c7fa
github username <github email address>
2017-02-21 10:45:51 -0500

    done

--- app.rb
+++ app.rb
@@ -8,9 +8,16 @@ class App < Sinatra::Base
   end
 
   # This is a sample dynamic route.
-  get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+  get '/goodbye/:name' do
+     @user_name = params[:name]
+     erb :goodbye
+    # "Goodbye #{@user_name}!"
+  end
+
+  get'/multiply/:num1/:num2'do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    erb :multiply
   end
 
 
